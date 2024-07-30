cb58637b334053113726b1ebf8c0ca6e87930f26
Krishnan Iyer <krishnaniyer97@gmail.com>
2024-07-30 14:47:54 +0300

    add revised axis async fifo

--- fpga/app/template/rtl/async_fifo.v
+++ fpga/app/template/rtl/async_fifo.v
@@ -12,96 +12,99 @@
  5. This code is not tested against odd frequencies of reader and writer which are  non-divisible
  */
 
-module fifo #
-  (
-   parameter integer DEPTH = 64,
-   parameter integer WIDTH = 512 // bits
-   )
-   (
-    input		     reset,
-    input		     wr_clk,
-    input		     rd_clk,
-    input		     wr_en,
-    input		     rd_en,
-    input [WIDTH-1 : 0]	     data_in,
-    output reg [WIDTH-1 : 0] data_out,
-    output reg		     fifo_full,
-    output reg		     fifo_empty,
-    output reg [31 : 0]	     ptr
-    );
+module async_fifo #
+(
+    parameter DEPTH = 64,
+    parameter WIDTH = 512 // bits
+)
+(
+    input		    wr_clk,
+    input		    wr_rst,
+    input		    wr_en,
+    input [WIDTH-1:0]	    data_in,
 
-   reg [WIDTH-1 : 0]	     mem [0 : DEPTH-1];
-   // read ptr is initialised to DEPTH-1 and write ptr to 0
-   // read ptr always lags 1 or more steps behind write ptr
-   // read ptr points to last read buffer and write ptr points
-   // to be written buffer
-   reg [31 : 0]		     rd_ptr = DEPTH-1;
-   reg [31 : 0]		     wr_ptr = 0;
+    input		    rd_clk,
+    input		    rd_rst,
+    input		    rd_en,
+    output wire [WIDTH-1:0] data_out,
+
+    output		    full,
+    output		    empty
+);
+
+parameter		   ADDR_WIDTH = $clog2(DEPTH);
+reg [WIDTH-1:0]		   mem [0:DEPTH-1];
+// read ptr is initialised to DEPTH-1 and write ptr to 0
+// read ptr always lags 1 or more steps behind write ptr
+// read ptr points to last read buffer and write ptr points
+// to be written buffer
+reg [ADDR_WIDTH-1:0]	   rd_ptr = 0;
+reg [ADDR_WIDTH-1:0]	   wr_ptr = 0;
+
+reg [ADDR_WIDTH-1:0]	   m_wr_rd_sync_ptr = {ADDR_WIDTH{1'b0}};
+reg [ADDR_WIDTH-1:0]	   s_wr_rd_sync_ptr = {ADDR_WIDTH{1'b0}};
+reg [ADDR_WIDTH-1:0]	   m_rd_wr_sync_ptr = {ADDR_WIDTH{1'b0}};
+reg [ADDR_WIDTH-1:0]	   s_rd_wr_sync_ptr = {ADDR_WIDTH{1'b0}};
+
+reg [ADDR_WIDTH-1:0]	   rd_sync_commit_ptr = {ADDR_WIDTH{1'b0}};
+reg [ADDR_WIDTH-1:0]	   wr_sync_commit_ptr = {ADDR_WIDTH{1'b0}};
+
+assign full = ((wr_ptr + 1'b1) == rd_ptr);
+assign empty =  wr_ptr == rd_ptr;
+
+assign data_out = rd_en ? mem[rd_ptr] : 0;
+
+reg			   wr_sync_rst = 1'b0;
+reg			   rd_sync_rst = 1'b0;
+reg			   wr_sync_rst_int = 1'b0;
+reg			   rd_sync_rst_int = 1'b0;
 
 
-   always @(posedge wr_clk) begin
-      if (reset) begin
-	 wr_ptr <= 0;
-      end
-      else if (wr_en) begin
-	 mem[wr_ptr] <= data_in;
-	 // implementation of circular buffer
-	 if(wr_ptr == (DEPTH - 1)) begin
-	    wr_ptr <= 0;
-	 end
-	 else begin
-	    wr_ptr <= wr_ptr + 1;
-	 end
-      end
-   end
+always @(posedge wr_clk) begin
+    if (wr_rst || (wr_sync_rst != wr_sync_rst_int)) begin
+	wr_ptr <= 0;
+	wr_sync_commit_ptr <= {ADDR_WIDTH{1'b0}};
+	if (wr_sync_rst != wr_sync_rst_int)
+	    wr_sync_rst_int <= wr_sync_rst;
+	else
+	    rd_sync_rst <= rd_sync_rst ^ 1'b1;
+    end
+    else begin
+	if (!full && wr_en) begin
+	    mem[wr_ptr] <= data_in;
+	    wr_ptr <= wr_ptr + 1'b1;
+	    m_wr_rd_sync_ptr <= wr_ptr + 1'b1;
+	end
+	if (m_wr_rd_sync_ptr == s_rd_wr_sync_ptr)
+	    wr_sync_commit_ptr <= m_wr_rd_sync_ptr;
+    end
+end
 
+always @(posedge rd_clk) begin
+    if (rd_rst || (rd_sync_rst != rd_sync_rst_int)) begin
+	rd_ptr <= {ADDR_WIDTH{1'b0}};
+	rd_sync_commit_ptr <= {ADDR_WIDTH{1'b0}};
+	if ((rd_sync_rst != rd_sync_rst_int))
+	    rd_sync_rst_int <= rd_sync_rst;
+	else
+	    wr_sync_rst <= wr_sync_rst ^ 1'b1;
+    end
+    else begin
+	if (!empty && rd_en) begin
+	    rd_ptr <= rd_ptr + 1'b1;
+	    m_rd_wr_sync_ptr <= rd_ptr + 1'b1;
+	end
+	if(m_rd_wr_sync_ptr == s_wr_rd_sync_ptr)
+	    rd_sync_commit_ptr <= m_rd_wr_sync_ptr;
+    end
+end
 
-   always @(posedge rd_clk) begin
-      if (reset) begin
-	 rd_ptr <= DEPTH-1;
-      end
-      else if (rd_en) begin
-	 data_out <= mem[rd_ptr];
-	 ptr <= rd_ptr;
-	 // TODO:
-	 // 1. make this optional
-	 // 2. reimplement with fewer signals, this might result to only allow track monitor
-	 //    module to only lag fewer steps behind.
-	  // edge case:
-	  // 1. Donot increment ahead to equal to write ptr
-	  // 2. handle for circular buffer case
-	  if (~(((rd_ptr + 1) == wr_ptr) || ((rd_ptr == (DEPTH - 1)) && (wr_ptr == 0)))) begin
-	      if( rd_ptr == (DEPTH - 1)) begin
-		  rd_ptr <= 0;
-	      end
-	      else begin
-		  rd_ptr <= rd_ptr + 1;
-	      end
-	  end
-      end
-   end
-
-   always @(posedge wr_clk or posedge rd_clk) begin
-      if (reset & wr_clk) begin
-	 fifo_full <= 0;
-	 fifo_empty <= 1;
-      end
-      else if(wr_clk & wr_en) begin
-	 // full if write ptr catches up with read ptr
-	 if((wr_ptr + 1) == rd_ptr) begin
-	    fifo_full <= 1'b1;
-	 end
-	 fifo_empty <= 1'b0;
-      end
-      else if(rd_clk & rd_en) begin
-	 // empty if read ptr catches up with write ptr
-	 if(((rd_ptr + 1) == wr_ptr) || ((rd_ptr == (DEPTH - 1)) && (wr_ptr == 0))) begin
-	    fifo_empty <= 1'b1;
-	 end
-	 fifo_full <= 1'b0;
-      end
-   end
-
+always @* begin
+    if(s_rd_wr_sync_ptr != m_wr_rd_sync_ptr)
+	s_rd_wr_sync_ptr = m_wr_rd_sync_ptr;
+    if (s_wr_rd_sync_ptr != m_rd_wr_sync_ptr)
+	s_wr_rd_sync_ptr = m_rd_wr_sync_ptr;
+end
 
 endmodule
 
---
+++ fpga/app/template/rtl/axis_async_fifo.v
@@ -0,0 +1,86 @@
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+module axis_async_fifo #
+(
+    parameter DATA_WIDTH = 8,
+    parameter KEEP_WIDTH = ((DATA_WIDTH+7)/8),
+    parameter FIFO_DEPTH = 8,
+    parameter ID_WIDTH = 8,
+    parameter DEST_WIDTH = 8,
+    parameter USER_WIDTH = 1
+)
+(
+    input wire			 s_clk,
+    input wire			 s_rst,
+    input wire [DATA_WIDTH-1:0]	 s_axis_tdata,
+    input wire [KEEP_WIDTH-1:0]	 s_axis_tkeep,
+    input wire			 s_axis_tvalid,
+    output wire			 s_axis_tready,
+    input wire			 s_axis_tlast,
+    input wire [ID_WIDTH-1:0]	 s_axis_tid,
+    input wire [DEST_WIDTH-1:0]	 s_axis_tdest,
+    input wire [USER_WIDTH-1:0]	 s_axis_tuser,
+
+    input wire			 m_clk,
+    input wire			 m_rst,
+    output wire [DATA_WIDTH-1:0] m_axis_tdata,
+    output wire [KEEP_WIDTH-1:0] m_axis_tkeep,
+    output wire			 m_axis_tvalid,
+    input wire			 m_axis_tready,
+    output wire			 m_axis_tlast,
+    output wire [ID_WIDTH-1:0]	 m_axis_tid,
+    output wire [DEST_WIDTH-1:0] m_axis_tdest,
+    output wire [USER_WIDTH-1:0] m_axis_tuser
+);
+
+localparam			 FIFO_WIDTH = DATA_WIDTH + KEEP_WIDTH + 1 + ID_WIDTH + DEST_WIDTH + USER_WIDTH; // tlast
+wire [FIFO_WIDTH-1:0]		 data_in;
+wire [FIFO_WIDTH-1:0]		 data_out;
+wire				 empty;
+wire				 full;
+
+assign s_axis_tready = full == 0 ? 1'b1 : 1'b0; // make ready false if fifo is false
+
+assign data_in = {s_axis_tdata, s_axis_tkeep, s_axis_tlast, s_axis_tid, s_axis_tdest, s_axis_tuser};
+
+assign m_axis_tdata = data_out[(FIFO_WIDTH-1) -: DATA_WIDTH]; // data_out [(fifo_width-1):(fifo_width-1)-512]
+assign m_axis_tkeep = data_out[(USER_WIDTH+DEST_WIDTH+ID_WIDTH+1)+: KEEP_WIDTH]; // data_out [1:65]
+assign m_axis_tlast = data_out[(USER_WIDTH+DEST_WIDTH+ID_WIDTH)];
+assign m_axis_tid =   data_out[(USER_WIDTH+DEST_WIDTH)+:ID_WIDTH];
+assign m_axis_tdest = data_out[USER_WIDTH+:DEST_WIDTH];
+assign m_axis_tuser = data_out[USER_WIDTH-1:0];
+
+// TODO: get arst and reset
+// always @(posedge m_clk) begin
+//     m_axis_tvalid <= (!empty) && m_axis_tready;
+// end
+
+assign m_axis_tvalid = (!empty) && m_axis_tready;
+
+async_fifo #
+(
+    .DEPTH(FIFO_DEPTH),
+    .WIDTH(FIFO_WIDTH)
+)
+fifo_inst
+(
+    .wr_clk(s_clk),
+    .wr_rst(s_rst),
+
+    .rd_clk(m_clk),
+    .rd_rst(m_rst),
+
+    .wr_en(s_axis_tvalid),
+    .rd_en(m_axis_tready),
+    .data_in(data_in),
+    .data_out(data_out),
+
+    .full(full),
+    .empty(empty)
+);
+
+endmodule
+
+`resetall
